b1beb92d4b8df02e8152adf14e3211cc45b8ee14
Ghany Abdillah Ersa <ghanyersa24@gmail.com>
2026-05-20 23:15:46 +0700

    fix: update base path in vite.config.js for correct deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ghany-folio/',
+  base: '/profile/',
   plugins: [react()],
   server: {
     port: 5173,
